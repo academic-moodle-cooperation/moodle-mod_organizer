e11c314c19ee97d5c4d0ec7f0746aa9c180fafc7
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2015-12-17 15:59:49 +0100

    version-file releasenumber corrected

--- version.php
+++ version.php
@@ -20,6 +20,7 @@
  * @package       mod_organizer
  * @author        Andreas Hruska (andreas.hruska@tuwien.ac.at)
  * @author        Katarzyna Potocka (katarzyna.potocka@tuwien.ac.at)
+ * @author        Thomas Niedermaier (thomas.niedermaier@meduniwien.ac.at)
  * @author        Andreas Windbichler
  * @author        Ivan Šakić
  * @copyright     2014 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
@@ -28,8 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015121700;
-$plugin->release = "2015-12-17"; // User-friendly version number.
+$plugin->version = 2015102001;
+$plugin->release = "2015-10-20 (1)"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2014041100; // Requires this Moodle version.
 $plugin->component = 'mod_organizer'; // Full name of the plugin (used for diagnostics).
