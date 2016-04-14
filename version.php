a48d68bd37c45c842ee3a93e04a621f278de32cb
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2016-04-14 10:48:31 +0200

    HOTFIX #3234: Students get notified constantly after the slot appointment is over

--- version.php
+++ version.php
@@ -29,8 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016012803;
-$plugin->release = "2016-01-28 (3)"; // User-friendly version number.
+$plugin->version = 2016012804;
+$plugin->release = "2016-01-28 (4)"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2014041100; // Requires this Moodle version.
 $plugin->component = 'mod_organizer'; // Full name of the plugin (used for diagnostics).
