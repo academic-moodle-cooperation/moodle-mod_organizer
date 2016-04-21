9477a87500f2f9a3c147de82926a5ce6ccdf5f31
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2016-04-21 13:04:01 +0200

    Changed $plugin->release to the new AMC-Standard v3.0.0

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2016041800;
-$plugin->release = "2016-04-18 (0)"; // User-friendly version number.
+$plugin->release = "v3.0.0"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2014041100; // Requires this Moodle version.
 $plugin->component = 'mod_organizer'; // Full name of the plugin (used for diagnostics).
