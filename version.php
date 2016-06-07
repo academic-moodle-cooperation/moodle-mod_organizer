b913757ffb431bad28df02c45e7898cc80d77e57
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2016-06-07 13:50:27 +0200

    new branch develop_31 from develop_30 generated

--- version.php
+++ version.php
@@ -29,8 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016050400;
-$plugin->release = "v3.0.0"; // User-friendly version number.
+$plugin->version = 2016060700;
+$plugin->release = "v3.1.0"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2014041100; // Requires this Moodle version.
 $plugin->component = 'mod_organizer'; // Full name of the plugin (used for diagnostics).
