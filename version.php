e37d1258fa53dc6042197de5172bc1534a7239c2
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2016-09-29 10:49:52 +0200

    Bug #3754 - forgot to increase the version

--- version.php
+++ version.php
@@ -29,8 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016072102;
-$plugin->release = "v3.1.2"; // User-friendly version number.
+$plugin->version = 2016072103;
+$plugin->release = "v3.1.3"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2014041100; // Requires this Moodle version.
 $plugin->component = 'mod_organizer'; // Full name of the plugin (used for diagnostics).
