d33db1ec6d0f46d6083838e2e1da3b387f5a22a6
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2017-01-30 07:28:58 +0100

    Bug #3997/2: Warning when DB-query does not contain a unique-ID in the first column Remove group by clause from view

--- view_lib.php
+++ view_lib.php
@@ -688,7 +688,7 @@ function organizer_organizer_organizer_get_status_table_entries_group($params) {
     $par = array_merge($par, $inparams);
 
     $query = "SELECT DISTINCT 
-        a2.id AS appid, g.id, g.name,
+        g.id, g.name,
         CASE
             WHEN a2.id IS NOT NULL AND a2.attended = 1 AND a2.allownewappointments = 0 
                 THEN " . ORGANIZER_APP_STATUS_ATTENDED . "
@@ -707,7 +707,7 @@ function organizer_organizer_organizer_get_status_table_entries_group($params) {
             ELSE " . ORGANIZER_APP_STATUS_INVALID . "
         END AS status, a2.starttime, a2.duration, a2.location, a2.locationlink,
         a2.teacherid, a2.applicantid, a2.teachercomments, a2.comments, a2.teachervisible,
-        a2.slotid, a2.allownewappointments, a2.teacherapplicantid, a2.teacherapplicanttimemodified
+        a2.slotid, a2.allownewappointments, a2.id AS appid, a2.teacherapplicantid, a2.teacherapplicanttimemodified
 
         FROM {groups} g
         LEFT JOIN
@@ -722,9 +722,6 @@ function organizer_organizer_organizer_get_status_table_entries_group($params) {
         INNER JOIN {organizer_slots} s ON a.slotid = s.id
         WHERE s.organizerid = :organizerid ORDER BY a.id DESC) a2 ON g.id = a2.groupid
         WHERE g.id $insql
-        GROUP BY g.id, g.name, status, a2.starttime, a2.duration, a2.location, a2.locationlink,
-            a2.teacherid, a2.applicantid, a2.teachercomments, a2.comments, a2.teachervisible, a2.slotid, 
-            a2.allownewappointments, a2.id, a2.teacherapplicantid, a2.teacherapplicanttimemodified
         $orderby";
 
 	$rs = $DB->get_records_sql($query, $par);
