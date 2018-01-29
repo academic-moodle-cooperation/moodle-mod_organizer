c27c4efc7142cc902b8ca1e2a909c3246616a96e
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2018-01-29 11:42:15 +0100

    version.php, changelog.txt

--- version.php
+++ version.php
@@ -29,8 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018011701;
-$plugin->release = "v3.4.0"; // User-friendly version number.
+$plugin->version = 2018012900;
+$plugin->release = "v3.4.1"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2017051200; // Requires this Moodle version.
 $plugin->component = 'mod_organizer'; // Full name of the plugin (used for diagnostics).
