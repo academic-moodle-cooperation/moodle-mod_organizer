5735f26289a3775a53ff51ca910abac7a31abd2c
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2018-08-02 12:59:08 +0200

    Increase version number

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018080100;
+$plugin->version = 2018080200;
 $plugin->release = "v3.5.0"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2018051700; // Requires this Moodle version.
