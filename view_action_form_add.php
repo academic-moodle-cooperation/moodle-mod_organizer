34c64406f810775e589613d999b320fba9f7abdc
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2019-05-05 21:49:14 +0200

    #6107: Edit a slot: location field not mandatory any more and an input field if suggestion list is empty / 2

--- view_action_form_add.php
+++ view_action_form_add.php
@@ -101,7 +101,6 @@ class organizer_add_slots_form extends moodleform
             $mform->setType('location', PARAM_RAW);
             $mform->setDefault('location', null);
         }
-        $mform->addRule('location', get_string('err_location', 'organizer'), 'required');
         $mform->addHelpButton('location', 'location', 'organizer');
 
         $mform->addElement('text', 'locationlink', get_string('locationlink', 'organizer'), array('size' => '64'));
