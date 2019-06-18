4ca3530af59d97fbe20deac1a696e085c21ed626
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2019-06-18 15:16:14 +0200

    Feature #5937: Privacy API - implement deletion

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019061303;
+$plugin->version = 2019061800;
 $plugin->release = "v3.7.0"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2018051700; // Requires this Moodle version.
