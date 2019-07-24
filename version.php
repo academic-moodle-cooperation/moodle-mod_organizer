fc537d91be5963de55fafc284ac62a47c71c311c
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2019-07-24 13:32:22 +0200

    Bug #6216: Calendar view: when organizer instance option "No calendar events for empty slots" is deactivated and a student books a slot, the calendar message changes the language / 2

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019072302;
+$plugin->version = 2019072400;
 $plugin->release = "v3.7.0"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2018051700; // Requires this Moodle version.
