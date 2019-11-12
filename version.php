feeede21f82d46a83e29e2981cc27a1fdfea107d
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2019-11-12 09:55:32 +0100

    correction version.php

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019092301;
+$plugin->version = 2019080701;
 $plugin->release = "v3.7.1"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2018051700; // Requires this Moodle version.
