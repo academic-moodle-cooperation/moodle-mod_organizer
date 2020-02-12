8ca8abf7088ff84beba05b6193e0653699a0c333
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2020-02-12 13:43:28 +0100

    Bring back Andreas ;-)

--- slots_delete.php
+++ slots_delete.php
@@ -21,6 +21,8 @@
  * @author    Andreas Hruska (andreas.hruska@tuwien.ac.at)
  * @author    Katarzyna Potocka (katarzyna.potocka@tuwien.ac.at)
  * @author    Thomas Niedermaier (thomas.niedermaier@meduniwien.ac.at)
+ * @author    Andreas Windbichler
+ * @author    Ivan Šakić
  * @copyright 2014 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
