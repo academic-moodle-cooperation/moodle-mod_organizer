ab3e81eddf7f5606b5044ae8310e685250ff6e76
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2022-06-17 05:06:54 +0200

    debugging...

--- view_action_form_edit.php
+++ view_action_form_edit.php
@@ -343,6 +343,37 @@ class organizer_edit_slots_form extends moodleform
             $mform->setType('mod_maxparticipants', PARAM_BOOL);
         }
 
+        $group = array();
+        $group[] = $mform->createElement('duration', 'availablefrom');
+        $group[] = $mform->createElement(
+            'static', '', '',
+            get_string('relative_deadline_before', 'organizer')
+        );
+        $group[] = $mform->createElement('checkbox', 'availablefrom[now]', get_string('relative_deadline_now', 'organizer'));
+        $group[] = $mform->createElement(
+            'static', '', '',
+            $this->_warning_icon('availablefrom', isset($defaults['availablefrom']))
+        );
+
+        $records = $DB->get_records('organizer_slots', null, null, 'id, organizerid, notificationtime');
+        foreach ($records as $record) {
+            var_dump($record);
+            echo "<br>----<br>";
+        }
+        die();
+
+        $mform->setDefault('availablefrom', '');
+        $mform->setDefault('availablefrom[now]', $defaults['now']);
+
+        $mform->addGroup($group, 'availablefromgroup', get_string('availablefrom', 'organizer'), ORGANIZER_SPACING, false);
+
+        $mform->disabledIf('availablefrom[number]', 'availablefrom[now]', 'checked');
+        $mform->disabledIf('availablefrom[timeunit]', 'availablefrom[now]', 'checked');
+
+        $availablefromgroup = $mform->getElement('availablefromgroup')->getElements();
+        $availablefrom = $availablefromgroup[0]->getElements();
+        $availablefrom[1]->removeOption(1);
+
         $mform->addElement('hidden', 'mod_availablefrom', 0);
         $mform->setType('mod_availablefrom', PARAM_BOOL);
 
