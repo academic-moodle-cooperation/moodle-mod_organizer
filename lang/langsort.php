97ff02bb04c6eb1c50c0b4af5261e2a12fe27d8a
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2023-02-04 22:14:19 +0100

    langsort 2

--- lang/langsort.php
+++ lang/langsort.php
@@ -27,7 +27,7 @@ require_once(dirname(__FILE__, 4) .'/config.php');
 
 $location = $CFG->dirroot . '/mod/organizer/lang/en/organizer.php';
 $firstseparator = "defined('MOODLE_INTERNAL') || die();";
-$lineseparator = ";";
+$lineseparator = "';";
 $fieldseparator = " = '";
 $inarr = array();
 $outstr = "";
@@ -43,20 +43,23 @@ if (count($arr) != 2) {
 $outstr = $arr[0].$linebreak.$firstseparator.$linebreak;
 $entries = explode($lineseparator, $arr[1]);
 
+$entrybefore = "";
 foreach ($entries as $entry) {
     $arr = explode($fieldseparator, $entry);
     if (count($arr) == 2) {
         $inarr[$arr[0]] = $arr[1];
     } else {
+        echo "*".$entrybefore."*<br>";
         echo "*".$entry."*<br>";
     }
+    $entrybefore = $entry;
 }
 
 ksort($inarr);
 
 $i = 0;
 foreach ($inarr as $key => $value) {
-    echo '#'.$key.$fieldseparator.$value.'#<br>';
+    echo $key.$fieldseparator.$value.$lineseparator.$linebreak;
     if ($i++ == 20) {
         break;
     }
