94bb18ca247cdd859ee28ddf81e828e0045070f0
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2023-02-04 21:58:52 +0100

    codechecker issue 2

--- lang/langsort.php
+++ lang/langsort.php
@@ -1,4 +1,28 @@
 <?php
+// This file is part of mod_organizer for Moodle - http://moodle.org/
+//
+// It is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// It is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * langsort.php
+ *
+ * @package       mod_organizer
+ * @author        Thomas Niedermaier (thomas.niedermaier@meduniwien.ac.at)
+ * @copyright     2022 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
+ * @license       http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 require_once(dirname(__FILE__, 4) .'/config.php');
 
 $location = $CFG->dirroot . '/mod/organizer/lang/en/organizer.php';
