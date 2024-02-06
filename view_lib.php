54d1751b1c5b3d204eb9ae99197708f10bd468e1
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2024-02-06 18:10:40 +0100

    change moduldescriptions to icons

--- view_lib.php
+++ view_lib.php
@@ -1899,37 +1899,47 @@ function organizer_get_img($src, $alt, $title, $id = '', $other = '') {
 function organizer_get_icon_msg($name, $infotxt) {
     switch ($name) {
         case 'group':
-            $out = organizer_get_fa_icon_stacked('fa-square-o fa-stack-2x', 'fa-group fa-stack-1x', $infotxt);
+            $out = organizer_get_fa_icon_stacked('fa-circle-thin fa-stack-2x text-primary',
+                'fa-group fa-stack-1x text-primary', $infotxt);
             break;
         case 'nogroup':
-            $out = organizer_get_fa_icon_stacked('fa-group fa-stack-1x', 'fa-exclamation fa-stack-2x text-danger', $infotxt);
+            $out = organizer_get_fa_icon_stacked('fa-group fa-stack-1x text-danger',
+                'fa-exclamation fa-stack-2x text-danger', $infotxt);
             break;
         case 'expires':
-            $out = organizer_get_fa_icon_stacked('fa-square-o fa-stack-2x', 'fa-hourglass-end fa-stack-1x', $infotxt);
+            $out = organizer_get_fa_icon_stacked('fa-circle-thin fa-stack-2x text-primary',
+                'fa-hourglass-3 fa-stack-1x text-primary', $infotxt);
             break;
         case 'expired':
-            $out = organizer_get_fa_icon_stacked('fa-hourglass fa-stack-1x', 'fa-ban fa-stack-2x', $infotxt);
+            $out = organizer_get_fa_icon_stacked('fa-clock-o fa-stack-1x text-danger',
+                'fa-circle-thin fa-stack-2x text-danger', $infotxt);
             break;
         case 'neverexpires':
-            $out = organizer_get_fa_icon_stacked('fa-square-o fa-stack-2x', 'fa-hourglass-o fa-stack-1x', $infotxt);
+            $out = organizer_get_fa_icon_stacked('fa-clock-o fa-stack-1x text-primary',
+                'fa-ban fa-stack-2x text-primary', $infotxt);
             break;
         case 'grade':
-            $out = organizer_get_fa_icon_stacked('fa-square-o fa-stack-2x', 'fa-th-list fa-stack-1x', $infotxt);
+            $out = organizer_get_fa_icon_stacked('fa-circle-thin fa-stack-2x text-primary',
+                'fa-th-list fa-stack-1x text-primary', $infotxt);
             break;
         case 'nograde':
-            $out = organizer_get_fa_icon_stacked('fa-th-list fa-stack-1x', 'fa-ban text-danger fa-stack-2x', $infotxt);
+            $out = organizer_get_fa_icon_stacked('fa-th-list fa-stack-1x text-primary',
+                'fa-ban fa-stack-2x text-primary', $infotxt);
             break;
         case 'queues':
-            $out = organizer_get_fa_icon_stacked('fa-square-o fa-stack-2x', 'fa-coffee fa-stack-1x', $infotxt);
+            $out = organizer_get_fa_icon_stacked('fa-circle-thin fa-stack-2x text-primary',
+                'fa-coffee fa-stack-1x text-primary', $infotxt);
             break;
         case 'noqueues':
-            $out = organizer_get_fa_icon_stacked('fa-coffee fa-stack-1x', 'fa-ban fa-stack-2x', $infotxt);
+            $out = organizer_get_fa_icon_stacked('fa-coffee fa-stack-1x text-primary',
+                'fa-ban fa-stack-2x text-primary', $infotxt);
             break;
         case 'minmax1':
-            $out = organizer_get_fa_icon_stacked('fa-square-o fa-stack-2x', 'fa-file-o fa-stack-1x', $infotxt);
+            $out = "";
             break;
         case 'minmax':
-            $out = organizer_get_fa_icon_stacked('fa-square-o fa-stack-2x', 'fa-files-o fa-stack-1x', $infotxt);
+            $out = organizer_get_fa_icon_stacked('fa-circle-thin fa-stack-2x text-primary',
+                    'fa-files-o fa-stack-1x text-primary', $infotxt).$infotxt;
             break;
     }
     return $out;
