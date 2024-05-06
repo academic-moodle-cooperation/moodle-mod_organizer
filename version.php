5776356f3c7cd505a8fa4001b207584bc804ccd9
Thomas Niedermaier <thomas.niedermaier@meduniwien.ac.at>
2024-05-06 10:35:46 +0200

    update Moodle version required

--- version.php
+++ version.php
@@ -33,5 +33,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2024041500;
 $plugin->release = "v4.4.0"; // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->requires = 2024040900; // Requires this Moodle version.
+$plugin->requires = 2024042200; // Requires this Moodle version.
 $plugin->component = 'mod_organizer'; // Full name of the plugin (used for diagnostics).
